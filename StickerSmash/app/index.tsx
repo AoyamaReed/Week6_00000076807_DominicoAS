537e586d53a1284f05622b9bc14dc404cdfc7a13
Dominico <toni8ahia@gmail.com>
2026-03-11 09:44:54 +0700

    first commit

---
+++ StickerSmash/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Dominico Anthony Susanto - 00000076807</Text>
+      <Text>IF670_00000076807_W01</Text>
+    </View>
+  );
+}
